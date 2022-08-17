0d2914af01a53c582ad3b4c4cef88cf77c6e181b
giovanna <giovanaaraujolima2019@gmail.com>
2022-08-17 12:23:47 -0300

    save

--- core/usuario_repositorio.php
+++ core/usuario_repositorio.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require_once '../includes/funçoes.php';
+    require_once '../includes/funcoes.php';
     require_once 'conexao_mysql.php';
     require_once 'sql.php';
     require_once 'mysql.php';
